d8688123a631025a896d38ab1b1580145b70d3b8
leandrita <lea.bujh@gmail.com>
2023-07-26 12:42:22 +0200

    update database name

--- model/BookModel.php
+++ model/BookModel.php
@@ -10,31 +10,31 @@ class model
     }
     public function insertar($titulo)
     {
-        $statement = $this->PDO->prepare("INSERT INTO CodeReaders VALUES(null,:titulo)");
+        $statement = $this->PDO->prepare("INSERT INTO codereaders VALUES(null,:titulo)");
         $statement->bindParam(":titulo", $titulo);
         return ($statement->execute()) ? $this->PDO->lastInsertId() : false;
     }
     public function show($id)
     {
-        $statement = $this->PDO->prepare("SELECT * FROM CodeReaders where id = :id limit 1");
+        $statement = $this->PDO->prepare("SELECT * FROM codereaders where id = :id limit 1");
         $statement->bindParam(":id", $id);
         return ($statement->execute()) ? $statement->fetch() : false;
     }
     public function index()
     {
-        $statement = $this->PDO->prepare("SELECT * FROM CodeReaders");
+        $statement = $this->PDO->prepare("SELECT * FROM codereaders");
         return ($statement->execute()) ? $statement->fetchAll() : false;
     }
     public function update($id, $titulo)
     {
-        $statement = $this->PDO->prepare("UPDATE CodeReaders SET titulo = :titulo WHERE id = :id");
+        $statement = $this->PDO->prepare("UPDATE codereaders SET titulo = :titulo WHERE id = :id");
         $statement->bindParam(":titulo", $titulo);
         $statement->bindParam(":id", $id);
         return ($statement->execute()) ? $id : false;
     }
     public function delete($id)
     {
-        $statement = $this->PDO->prepare("DELETE FROM CodeReaders WHERE id = :id");
+        $statement = $this->PDO->prepare("DELETE FROM codereaders WHERE id = :id");
         $statement->bindParam(":id", $id);
         return ($statement->execute()) ? true : false;
     }
