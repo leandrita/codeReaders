05961b729c90f44faf47da989889195351063456
leandrita <lea.bujh@gmail.com>
2023-07-26 16:01:58 +0200

    styles index

--- view/head/head.php
+++ view/head/head.php
@@ -11,27 +11,29 @@
             : ((strpos($_SERVER['REQUEST_URI'], 'show')) ? "Detalles del libro" . $_GET['id'] : "Actualizar libro" . $_GET['id']);
         ?>
     </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
 </head>
 
 <body>
-    <div class="container-fluid bg-dark p-2 mb-3">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Logo</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="/codeReaders/index.php">Inicio</a>
-                        <a class="nav-link" href="/codeReaders/view/book/create.php">Crear</a>
-                    </div>
-                </div>
+    <div class="container">
+        <nav class="navbar navbar-expand-md">
+            <a class="navbar-brand" href="#">
+                <img class="logo" src="resources/img/Logo.png" alt="Logo" width="150" height="150">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/codeReaders/index.php">INICIO</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/codeReaders/view/book/create.php" onclick="abrirModal()">CREAR</a>
+                    </li>
+                </ul>
             </div>
         </nav>
     </div>
-    <div class="container-fluid">
+    <div class=" container-fluid">
