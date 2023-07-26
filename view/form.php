85b905e8a88e0123d3ce8b580dd7ca559845bef5
LucíaSilva <coderalsc@gmail.com>
2023-07-26 15:16:04 +0200

    estilos de la página

--- view/form.php
+++ view/form.php
@@ -4,37 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario</title>
-    <link rel="stylesheet" href="../resources/css/form.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="../resources/css/form.css"
 </head>
 <body>
+    <div class="container">
+        <div class="form-container">
+            <form action="../controller/create.php" method="post" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="title">Título</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="author">Autor</label>
+                    <input type="text" class="form-control" id="author" name="author" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="description">Descripción</label>
+                    <input type="text" class="form-control" id="description" name="description" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="isbn">ISBN</label>
+                    <input type="text" class="form-control" id="isbn" name="isbn" aria-describedby="isbnHelp" placeholder="">
+                    <small id="isbnHelp" class="form-text text-muted">Introduce únicamente números.</small>
+                </div>
+                <div class="form-group">
+                    <label for="image">Sube una imagen:</label>
+                    <input type="file" class="form-control-file" id="image" name="image">
+                </div>
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </form>
+        </div>
+    </div>
 
-    <form action="../controller/create.php" method="post" enctype="multipart/form-data">
-        <label for="title">Título</label>
-        <input type="text" class="form-control" id="title" name="title" placeholder="">
-    </div>
-    <div class="form-group">
-        <label for="author">Autor</label>
-        <input type="text" class="form-control" id="author" name="author" placeholder="">
-    </div>
-    <div class="form-group">
-        <label for="description">Descripción</label>
-        <input type="text" class="form-control" id="description" name="description" placeholder="">
-    </div>
-    <div class="form-group">
-        <label for="isbn">ISBN</label>
-        <input type="text" class="form-control" id="isbn" name="isbn" aria-describedby="isbnHelp" placeholder="">
-        <small id="isbnHelp" class="form-text text-muted">Introduce únicamente números.</small>
-    </div>
-    <div class="form-group">
-        <label for="image">Sube una imagen:</label>
-        <input type="file" class="form-control-file" id="image" name="image">
-    </div>
-    <button type="submit" class="btn btn-primary">Enviar</button>
-</form>
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!-- Los scripts de Bootstrap y jQuery van después del </body> para mejorar el rendimiento -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
