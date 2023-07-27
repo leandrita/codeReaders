e62ec529634ca7d878cedb5ba40885a6fa27be30
leandrita <lea.bujh@gmail.com>
2023-07-27 23:49:30 +0200

    styles create page

--- view/book/create.php
+++ view/book/create.php
@@ -38,32 +38,37 @@ ini_set('display_errors', 1);
             </div>
         </nav>
     </div>
-    <form action="/codeReaders/view/book/store.php" method="POST" enctype="multipart/form-data" autocomplete="off">
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Título</label>
-            <input type="text" name="titulo" required class="form-control" id="exampleInputEmail1"
-                aria-describedby="emailHelp">
-        </div>
-        <div class="form-group">
-            <label for="author">Autor</label>
-            <input type="text" class="form-control" id="author" name="autor" placeholder="">
-        </div>
-        <div class="form-group">
-            <label for="description">Descripción</label>
-            <input type="text" class="form-control" id="description" name="descripcion" placeholder="">
-        </div>
-        <div class="form-group">
-            <label for="isbn">ISBN</label>
-            <input type="text" class="form-control" id="isbn" name="isbn" aria-describedby="isbnHelp" placeholder="">
-            <small id="isbnHelp" class="form-text text-muted">Introduce únicamente números.</small>
-        </div>
-        <div class="form-group">
-            <label for="image">Sube una imagen:</label>
-            <input type="file" class="form-control-file" id="image" name="imagen">
-        </div>
-        <button type="submit" class="btn btn-primary">Guardar</button>
-        <a href="/codeReaders/index.php" class="btn btn-danger">Cancelar</a>
-    </form>
+    <div class="container vh-100">
+        <form action="/codeReaders/view/book/store.php" method="POST" enctype="multipart/form-data" autocomplete="off">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Título</label>
+                <input type="text" name="titulo" required class="form-control" id="exampleInputEmail1"
+                    aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+                <label for="author">Autor</label>
+                <input type="text" class="form-control" id="author" name="autor" placeholder="">
+            </div>
+            <div class="form-group">
+                <label for="description">Descripción</label>
+                <input type="text" class="form-control" id="description" name="descripcion" placeholder="">
+            </div>
+            <div class="form-group">
+                <label for="isbn">ISBN</label>
+                <input type="text" class="form-control" id="isbn" name="isbn" aria-describedby="isbnHelp"
+                    placeholder="">
+                <small id="isbnHelp" class="form-text text-muted">Introduce únicamente números.</small>
+            </div>
+            <div class="form-group">
+                <label for="image" class="btn btn-primary btn-file">
+                    Elegir imagen<input type="file" id="image" name="imagen" style="display: none;">
+                </label>
+
+            </div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+            <a href="/codeReaders/index.php" class="btn btn-danger">Cancelar</a>
+        </form>
+    </div>
 
     <?php
     require_once("/Applications/MAMP/htdocs/codeReaders/view/head/footer.php");
