0a013c704c9c8f117da6c6b00b67230a413862f1
leandrita <lea.bujh@gmail.com>
2023-07-27 22:04:27 +0200

    add search function

--- view/head/head.php
+++ view/head/head.php
@@ -40,8 +40,8 @@
         <div class="container mt-5">
             <form method="post">
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" id="codereaders" name="codereaders" placeholder=""
-                        aria-label="Buscar" aria-describedby="button-addon2">
+                    <input type="text" class="form-control" id="codereaders" name="codereaders"
+                        placeholder="Buscar por Título o Autor" aria-label="Buscar" aria-describedby="button-addon2">
                     <div class="input-group-append">
                         <button class="btn btn-primary" type="submit" name="buscar" id="button-addon2">Buscar</button>
                     </div>
@@ -53,14 +53,14 @@
 
             if (isset($_POST["buscar"])) {
                 $str = $_POST["codereaders"];
-                $sth = $con->prepare('SELECT * FROM codereaders WHERE titulo = :titulo');
-                $sth->bindParam(':titulo', $str, PDO::PARAM_STR);
+                $sth = $con->prepare('SELECT * FROM codereaders WHERE titulo LIKE :search OR autor LIKE :search');
+                $searchParam = '%' . $str . '%';
+                $sth->bindParam(':search', $searchParam, PDO::PARAM_STR);
                 $sth->setFetchMode(PDO::FETCH_OBJ);
                 $sth->execute();
-                $row = $sth->fetch();
-                // $rows = $sth->fetchAll(PDO::FETCH_OBJ);
-            
-                if ($row) {
+                $rows = $sth->fetchAll(PDO::FETCH_OBJ);
+
+                if (count($rows) > 0) {
                     ?>
                     <div class="row">
                         <div class="col">
@@ -74,66 +74,59 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php if ($row): ?>
-                                        <?php foreach ($row as $book): ?>
-                                            <tr>
-                                                <td>
-                                                    <?php echo $book->titulo; ?>
-                                                </td>
-                                                <td>
-                                                    <?php echo $book->autor; ?>
-                                                </td>
+                                    <?php foreach ($rows as $book): ?>
+                                        <tr>
+                                            <td>
+                                                <?php echo $book->titulo; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $book->autor; ?>
+                                            </td>
 
-                                                <th class="tapa">
-                                                    <img class="card-img-top"
-                                                        src="data:image;base64,<?php echo base64_encode($book->imagen); ?>">
-                                                </th>
-                                                <th class="d-flex flex-column">
-                                                    <a href="/codeReaders/view/book/edit.php?id=<?= $row[0] ?>"
-                                                        class="btn btn-primary">Editar</a>
-                                                    <!-- Button trigger modal -->
-                                                    <a class="btn btn-danger" data-bs-toggle="modal"
-                                                        data-bs-target="#exampleModal">Eliminar</a>
-                                                    <a href="view/book/show.php?id=<?= $row[0] ?>" class="btn btn-info">+ Info</a>
-                                                </th>
-                                                <!-- Modal -->
-                                                <div class="modal fade" id="exampleModal" tabindex="-1"
-                                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                    <div class="modal-dialog">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Desea eliminar el
-                                                                    registro?</h1>
-                                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                                    aria-label="Close"></button>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                Una vez eliminado no se podrá recuperar el registro.
-                                                            </div>
-                                                            <div class="modal-footer">
-                                                                <button type="button" class="btn btn-primary"
-                                                                    data-bs-dismiss="modal">Cerrar</button>
-                                                                <a href="view/book/delete.php?id=<?= $row[0] ?>"
-                                                                    class="btn btn-danger">Eliminar</a>
-                                                            </div>
-                                                        </div>
+                                            <td class="tapa">
+                                                <img class="card-img-top"
+                                                    src="data:image;base64,<?php echo base64_encode($book->imagen); ?>">
+                                            </td>
+                                            <td class="d-flex flex-column">
+                                                <a href="/codeReaders/view/book/edit.php?id=<?= $book->id ?>"
+                                                    class="btn btn-primary">Editar</a>
+                                                <!-- Button trigger modal -->
+                                                <a class="btn btn-danger" data-bs-toggle="modal"
+                                                    data-bs-target="#exampleModal_<?= $book->id ?>">Eliminar</a>
+                                                <a href="view/book/show.php?id=<?= $book->id ?>" class="btn btn-info">+ Info</a>
+                                            </td>
+                                        </tr>
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="exampleModal_<?= $book->id ?>" tabindex="-1"
+                                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Desea eliminar el
+                                                            registro?</h1>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                            aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        Una vez eliminado no se podrá recuperar el registro.
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-primary"
+                                                            data-bs-dismiss="modal">Cerrar</button>
+                                                        <a href="view/book/delete.php?id=<?= $book->id ?>"
+                                                            class="btn btn-danger">Eliminar</a>
                                                     </div>
                                                 </div>
-                                                </th>
-                                            </tr>
-                                        <?php endforeach; ?>
-                                    <?php else: ?>
-                                        <tr>
-                                            <td colspan="3" class="text-center">No hay registros</td>
-                                        </tr>
-                                    <?php endif; ?>
+                                            </div>
+                                        </div>
+                                    <?php endforeach; ?>
                                 </tbody>
                             </table>
                         </div>
                     </div>
                     <?php
                 } else {
-                    echo '<div class="row"><div class="col"><p>Título no existe</p></div></div>';
+                    echo '<div class="row"><div class="col"><p>No se encontraron resultados</p></div></div>';
                 }
             }
             ?>
