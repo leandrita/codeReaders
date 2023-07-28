f4d23861d089f5c9b96bf59da6298f3b48e2f294
Ana Lucía Silva Córdoba <131855670+alusilco@users.noreply.github.com>
2023-07-28 08:52:45 +0200

    Update head.php
    modificación de enlaces para que no desaparezcan

--- view/head/head.php
+++ view/head/head.php
@@ -24,7 +24,7 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
                         <a class="nav-link" href="/codeReaders/index.php">INICIO</a>
